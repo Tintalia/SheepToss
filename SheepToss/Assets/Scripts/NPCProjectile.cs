81d45f8cf2c39a31a90a63eb94fd4798aa7498fe
Silvia Slavcheva <silvia.b.slavcheva@gmail.com>
2014-10-15 19:04:22 +0300

    Added Arrow

--- SheepToss/Assets/Scripts/NPCProjectile.cs
+++ SheepToss/Assets/Scripts/NPCProjectile.cs
@@ -6,7 +6,20 @@ class NPCProjectile : Projectile
 {
     public NPCProjectileType type { get; set; }
 
+    public int Attack { get; set; }
+    public float speed = 10;
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(this.gameObject);
+    }
+
     public override void Move()
     {
+    }
+
+    void Start()
+    {
+        rigidbody2D.velocity = -transform.right * speed;
     }
 }
--- SheepToss/Assets/Scripts/Sheppard.cs
+++ SheepToss/Assets/Scripts/Sheppard.cs
@@ -5,8 +5,20 @@ using System;
 class Sheppard : NonPlayerCharacter, ILivable
 {
     public float HP { get; set; }
-
+    public Transform arrow;
+    public Transform arrowSpawn;
+    public float rateOfFire;
+    private float nextFire;
     public override void Move()
     {
+       
+    }
+    public void Update()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + rateOfFire;
+            Instantiate(arrow, arrowSpawn.position, arrowSpawn.rotation);
+        }
     }
 }
